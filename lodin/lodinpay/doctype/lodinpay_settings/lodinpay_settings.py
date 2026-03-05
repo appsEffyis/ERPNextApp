7019d15123c6df32023cd80cf080540014cd8606
appsEffyis <apps@effyis-partners.ma>
2026-03-05 09:31:01 +0000

    J'ai supprimé le dossier lodin

--- lodin/lodinpay/doctype/lodinpay_settings/lodinpay_settings.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2026, Moi and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-
-class LodinPaySettings(Document):
-	pass
